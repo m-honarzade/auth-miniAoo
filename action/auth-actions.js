838089f5620a6e0323c7f736e846ae052ad93307
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-10-30 11:19:13 +0330

    adding creating user functionality, and hashing password

--- action/auth-actions.js
+++ action/auth-actions.js
@@ -1,4 +1,9 @@
 "use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -7,10 +12,25 @@ export async function signup(prevState, formData) {
     errors.email = "please enter a valid email address.";
   }
   if (password.trim().length < 8) {
-    errors.email = "please enter a valid password with at least 8 characters.";
+    errors.password =
+      "please enter a valid password with at least 8 characters.";
   }
 
   if (Object.keys(errors).length > 0) {
     return { errors: errors };
   }
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "It seems like an account for the chosen email already exist.",
+        },
+      };
+    }
+    throw error;
+  }
+  redirect("/training");
 }
